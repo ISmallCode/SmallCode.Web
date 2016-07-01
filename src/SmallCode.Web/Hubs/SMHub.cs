3a1bcfbea9932686088869deccea5520439527bc
applenele <1941973283@qq.com>
2016-07-02 00:04:08 +0800

    rtm

--- src/SmallCode.Web/Hubs/SMHub.cs
+++ src/SmallCode.Web/Hubs/SMHub.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿//using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 
 namespace SmallCode.Web.Hubs
 {
-    public class SMHub : Hub
-    {
-        public override Task OnConnected()
-        {
-            return base.OnConnected();
-        }
+    //public class SMHub : Hub
+    //{
+    //    public override Task OnConnected()
+    //    {
+    //        return base.OnConnected();
+    //    }
 
-        public override Task OnDisconnected(bool stopCalled)
-        {
-            return base.OnDisconnected(stopCalled);
-        }
+    //    public override Task OnDisconnected(bool stopCalled)
+    //    {
+    //        return base.OnDisconnected(stopCalled);
+    //    }
 
 
-        public void Send(string name, object message, string cid)
-        {
-            Clients.All.Send(name, message);
-        }
-    }
+    //    public void Send(string name, object message, string cid)
+    //    {
+    //        Clients.All.Send(name, message);
+    //    }
+    //}
 }
--- src/SmallCode.Web/Startup.cs
+++ src/SmallCode.Web/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using SmallCode.Web.Services;
 using SmallCode.Web.Services.Impl;
-using Microsoft.AspNetCore.SignalR.Infrastructure;
+//using Microsoft.AspNetCore.SignalR.Infrastructure;
 
 namespace SmallCode.Web
 {
@@ -48,11 +48,11 @@ namespace SmallCode.Web
             //csrf
             services.AddAntiforgery();
             services.AddCors();
-            services.AddSignalR(options =>
-            {
-                options.Hubs.EnableDetailedErrors = true;
-            });
-            services.AddSingleton<ConnectionManager>();
+            //services.AddSignalR(options =>
+            //{
+            //    options.Hubs.EnableDetailedErrors = true;
+            //});
+            //services.AddSingleton<ConnectionManager>();
 
             ///注入services
             services.AddScoped<IUserService, UserService>();
@@ -77,7 +77,7 @@ namespace SmallCode.Web
             app.UseStaticFiles();
 
             app.UseSession();
-            app.UseSignalR();
+           // app.UseSignalR();
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AutomaticAuthenticate = true,
